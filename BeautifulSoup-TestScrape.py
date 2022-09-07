4e35978ef0f20a74aea351e74a4fb62cff6e026c
Punguinz <PenguinBusiness01@gmail.com>
2022-09-06 22:41:00 -0400

    Further Testing Beautiful Soup

--- BeautifulSoup-TestScrape.py
+++ BeautifulSoup-TestScrape.py
@@ -1,7 +1,9 @@
-print("Test")
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
 
-def poop:
-    print("teet")
-    return
 
-poop()
+
+with open("PlusPortals-Scraper/testhtml.html") as website:
+    soup = bs(website, "html.parser")
+tag = soup.b    
+print(str(tag.name))
