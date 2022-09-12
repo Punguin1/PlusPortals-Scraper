379cfada6fc6f3785ebd1be7fea1d1c62080794e
Punguinz <PenguinBusiness01@gmail.com>
2022-09-12 17:02:06 -0400

    Major update to code

--- BeautifulSoup-TestScrape.py
+++ BeautifulSoup-TestScrape.py
@@ -1,9 +1,19 @@
-from bs4 import BeautifulSoup as bs
-from urllib.request import urlopen as uReq
+from gettext import find
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
 
+TAG_RE = re.compile(r'<[^>]+>')
 
+def remove_tags(text):
+    return TAG_RE.sub('', text)
 
-with open("PlusPortals-Scraper/testhtml.html") as website:
-    soup = bs(website, "html.parser")
-tag = soup.b    
-print(str(tag.name))
+website = "https://plusportals.com/doradoacademy"
+
+page = urlopen(website)
+pageTextBytes = page.read()
+poodoc = pageTextBytes.decode("utf-8")
+soup = BeautifulSoup(poodoc, "html.parser")
+tag = soup.find("h1")
+
+print(remove_tags(str(tag)))
