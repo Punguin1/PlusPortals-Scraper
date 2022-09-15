9bb943dc14c8cbd6e865c8eabe320ffd47b9b360
Punguinz <PenguinBusiness01@gmail.com>
2022-09-14 23:05:25 -0400

    Selenium Switch
    Possibly switching over to selenium for the scraper due to missing json html that Requests returns

--- PlusPortalsScrape.py
+++ PlusPortalsScrape.py
@@ -1,4 +1,4 @@
-from PlusportalsLogin import loginPayload
+from PlusportalsLogin import sendLogin
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
@@ -7,9 +7,12 @@ TAG_RE = re.compile(r'<[^>]+>')
 def remove_tags(text):
     return TAG_RE.sub('', text)
 
-website = loginPayload("2024jcrivera@doradoacademy.org", "Hrengin1")
+
+url = "https://plusportals.com/doradoacademy"
+
+website = sendLogin("2024jcrivera@doradoacademy.org", "Hrengin1", url)
 soup = BeautifulSoup(website.text, "html.parser")
-print(website.url)
+print(website)
 """webTable = soup.find(id="GridRecentScore")
 textExtract = soup.find(role="gridcell")
 print(textExtract)"""
--- PlusportalsLogin.py
+++ PlusportalsLogin.py
@@ -1,14 +1,26 @@
-import pip._vendor.requests as requests
+import requests
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-def loginPayload(username, password):
+def sendLogin(username, password, url):
     payload = {
         "User-agent": "*",
         "username": username,
         "password": password
     }
-    r = requests.post("https://plusportals.com/doradoacademy", data=payload)
+
+
+
+    PATH = "C:\Program Files (x86)\PYTHON DEPENDENCIES\chromedriver.exe"
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    wdriver = webdriver.Chrome(executable_path=PATH, options=chrome_options)
+    wdriver.get(url)
+    soup = BeautifulSoup(wdriver.page_source.encode("utf-8"), "html.parser")
+    sleep(3)
     fp = open('PlusPortals-Scraper/HTMLplusportals.html', 'w')
-    fp.write(r.text)
+    fp.write(str(soup.prettify))
     fp.close
-    return r
+    return soup
 
